09e0781f47b2d2e2c5979889520302497d58bbe7
patpet21 <96307724+patpet21@users.noreply.github.com>
2026-08-20 10:52:01 -0400

    Install Protect quick actions after redesign

--- src/ai_pm_lab_privacy_gate/ui/__init__.py
+++ src/ai_pm_lab_privacy_gate/ui/__init__.py
@@ -1,8 +1,10 @@
 from .mcp_log_guard import install_mcp_log_guard
 from .redesign import install_redesign
+from .protect_quick_actions import install_protect_quick_actions
 
 install_mcp_log_guard()
 install_redesign()
+install_protect_quick_actions()
 
 from .main_window import MainWindow
 
