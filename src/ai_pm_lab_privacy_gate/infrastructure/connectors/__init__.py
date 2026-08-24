0abf89d5ef455691447b1e33a9391891a295994f
patpet21 <96307724+patpet21@users.noreply.github.com>
2026-08-24 19:47:52 -0400

    Install multi-provider OAuth adapter

--- src/ai_pm_lab_privacy_gate/infrastructure/connectors/__init__.py
+++ src/ai_pm_lab_privacy_gate/infrastructure/connectors/__init__.py
@@ -1,6 +1,8 @@
 from .service import ConnectedAppsService, ConnectionTestResult, RemoteItem
 from .google_oauth_adapter import install_google_oauth_adapter
+from .multi_oauth_adapter import install_multi_oauth_adapter
 
 install_google_oauth_adapter()
+install_multi_oauth_adapter()
 
 __all__ = ["ConnectedAppsService", "ConnectionTestResult", "RemoteItem"]
