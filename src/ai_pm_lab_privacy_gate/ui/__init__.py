eb6d02581aec98ba6599a8ba8fcb231415a633ba
patpet21 <96307724+patpet21@users.noreply.github.com>
2026-08-24 17:42:53 -0400

    Install final Protect and Restore visual polish

--- src/ai_pm_lab_privacy_gate/ui/__init__.py
+++ src/ai_pm_lab_privacy_gate/ui/__init__.py
@@ -1,10 +1,12 @@
 from .mcp_log_guard import install_mcp_log_guard
 from .redesign import install_redesign
 from .protect_quick_actions import install_protect_quick_actions
+from .layout_polish import install_layout_polish
 
 install_mcp_log_guard()
 install_redesign()
 install_protect_quick_actions()
+install_layout_polish()
 
 from .main_window import MainWindow
 
