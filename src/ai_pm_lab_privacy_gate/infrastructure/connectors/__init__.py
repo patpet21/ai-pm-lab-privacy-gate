de87ed53efe6cfbc70b36f58e11d20c6ff04b61f
patpet21 <96307724+patpet21@users.noreply.github.com>
2026-08-25 11:07:35 -0400

    Install extended project platform connectors

--- src/ai_pm_lab_privacy_gate/infrastructure/connectors/__init__.py
+++ src/ai_pm_lab_privacy_gate/infrastructure/connectors/__init__.py
@@ -3,10 +3,12 @@ from .google_oauth_adapter import install_google_oauth_adapter
 from .multi_oauth_adapter import install_multi_oauth_adapter
 from .search_adapter import install_search_adapter
 from .clickup_adapter import install_clickup_adapter
+from .project_platform_adapter import install_project_platform_adapter
 
 install_google_oauth_adapter()
 install_multi_oauth_adapter()
 install_search_adapter()
 install_clickup_adapter()
+install_project_platform_adapter()
 
 __all__ = ["ConnectedAppsService", "ConnectionTestResult", "RemoteItem"]
