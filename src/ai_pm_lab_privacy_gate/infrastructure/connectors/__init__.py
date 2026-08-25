c9a8d00590a34110d34c0183ba816b17ba592149
patpet21 <96307724+patpet21@users.noreply.github.com>
2026-08-25 12:43:34 -0400

    Install automatic Notion OAuth flow

--- src/ai_pm_lab_privacy_gate/infrastructure/connectors/__init__.py
+++ src/ai_pm_lab_privacy_gate/infrastructure/connectors/__init__.py
@@ -5,6 +5,7 @@ from .search_adapter import install_search_adapter
 from .clickup_adapter import install_clickup_adapter
 from .project_platform_adapter import install_project_platform_adapter
 from .asana_auto_oauth import install_asana_auto_oauth
+from .notion_auto_oauth import install_notion_auto_oauth
 
 install_google_oauth_adapter()
 install_multi_oauth_adapter()
@@ -12,5 +13,6 @@ install_search_adapter()
 install_clickup_adapter()
 install_project_platform_adapter()
 install_asana_auto_oauth()
+install_notion_auto_oauth()
 
 __all__ = ["ConnectedAppsService", "ConnectionTestResult", "RemoteItem"]
