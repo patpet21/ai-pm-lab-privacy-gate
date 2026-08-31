78bf3f1a0ce80033a9867f6ad0a2fa22ff216982
patpet21 <96307724+patpet21@users.noreply.github.com>
2026-08-31 19:07:44 -0400

    Add English detector guardrail package

---
+++ src/ai_pm_lab_privacy_gate/infrastructure/pii/recognizers/english/__init__.py
@@ -0,0 +1,11 @@
+from .guardrails import (
+    email_linked_person_findings,
+    filter_english_ner_results,
+    propagate_known_ner_values,
+)
+
+__all__ = (
+    "email_linked_person_findings",
+    "filter_english_ner_results",
+    "propagate_known_ner_values",
+)
