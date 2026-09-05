11554683412ebd8d5659478e7a5658a63445fbea
patpet21 <96307724+patpet21@users.noreply.github.com>
2026-09-05 11:47:05 -0400

    fix: enable shared restore recovery patch

--- browser_extension/chromium_freev1_poc/browser_background.js
+++ browser_extension/chromium_freev1_poc/browser_background.js
@@ -1,4 +1,5 @@
 importScripts("background.js");
+importScripts("restore_recovery_patch.js");
 importScripts("text_review_background.js");
 importScripts("browser_docx_bridge.js");
 importScripts("browser_restore_auto_bridge.js");
