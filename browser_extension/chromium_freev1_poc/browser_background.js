57ae794b315c74a7760b63309cdfca9d8702b963
patpet21 <96307724+patpet21@users.noreply.github.com>
2026-09-05 14:01:23 -0400

    refactor: use unified browser file bridge

--- browser_extension/chromium_freev1_poc/browser_background.js
+++ browser_extension/chromium_freev1_poc/browser_background.js
@@ -1,5 +1,5 @@
 importScripts("background.js");
 importScripts("restore_recovery_patch.js");
 importScripts("text_review_background.js");
-importScripts("browser_docx_bridge.js");
+importScripts("browser_file_bridge.js");
 importScripts("browser_restore_auto_bridge.js");
